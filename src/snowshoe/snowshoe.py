03fcb59729c4f58bbafee9a5a0c765e71682bcbc
Amin Fazlali <aminfa92@gmail.com>
2024-01-02 12:36:04 +0330

    bug fix

--- src/snowshoe/snowshoe.py
+++ src/snowshoe/snowshoe.py
@@ -1,9 +1,13 @@
+import functools
 import json
+import threading
 from enum import Enum
+from threading import Thread
 from typing import Callable
 
 import pika
 from pika import exceptions
+from pika.exchange_type import ExchangeType
 from retry import retry
 
 
@@ -20,11 +24,11 @@ class Queue:
 
     name: str
     bindings: list[QueueBinding]
-    passive=False
-    durable=False
-    exclusive=False
-    auto_delete=False
-    arguments=None
+    passive = False
+    durable = False
+    exclusive = False
+    auto_delete = False
+    arguments = None
 
     def __init__(
             self,
@@ -47,8 +51,8 @@ class Queue:
 
 class AckMethod(Enum):
     OFF = 0
-    ON_FETCH = 1
-    ON_SUCCESS = 2
+    INSTANTLY = 1
+    AUTO = 2
 
 
 class Message:
@@ -78,11 +82,18 @@ class Snowshoe:
             )
         ))
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(self.name, 'topic')
+        self.channel.exchange_declare(self.name, ExchangeType.topic)
+        self._threads: list[Thread] = []
 
     @retry(exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
-    def start(self):
-        self.channel.start_consuming()
+    def run(self):
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        for thread in self._threads:
+            thread.join()
 
     def define_queues(self, queues: list[Queue]):
         for queue in queues:
@@ -96,29 +107,50 @@ class Snowshoe:
                 arguments=queue.arguments
             )
             for binding in queue.bindings:
+                self.channel.exchange_declare(binding.exchange, ExchangeType.topic)
                 self.channel.queue_bind(queue_name, binding.exchange, binding.routing_key)
 
     def emit(self, topic: str, data: dict):
         return self.channel.basic_publish(exchange=self.name, routing_key=topic, body=json.dumps(data).encode())
 
-    def on(self, queue: str | Queue, ack_method: AckMethod = AckMethod.ON_SUCCESS):
+    def ack(self, delivery_tag: int = 0, multiple=False):
+        self.channel.connection.add_callback_threadsafe(functools.partial(self.channel.basic_ack, delivery_tag, multiple))
+
+    def nack(self, delivery_tag: int = 0, multiple=False, requeue=True):
+        self.channel.connection.add_callback_threadsafe(functools.partial(self.channel.basic_nack, delivery_tag, multiple, requeue))
+
+    def on(self, queue: str | Queue, ack_method: AckMethod = AckMethod.AUTO):
         if isinstance(queue, Queue):
             queue = self.name + ':' + queue.name
         else:
             queue = self.name + ':' + queue
 
         def wrapper(handler: Callable[[Message], any]):
-            def callback(
+
+            def do_works(
                     _channel: pika.adapters.blocking_connection.BlockingChannel,
                     method: pika.spec.Basic.Deliver,
                     _properties: pika.spec.BasicProperties,
                     body: bytes
             ):
-                result = handler(Message(data=json.loads(body), topic=method.routing_key, delivery_tag=method.delivery_tag))
-                if ack_method == AckMethod.ON_SUCCESS:
-                    self.channel.basic_ack(method.delivery_tag)
-                return result
+                try:
+                    print(threading.get_ident())
+                    message = Message(data=json.loads(body), topic=method.routing_key, delivery_tag=method.delivery_tag)
+                    result = handler(message)
+                    if ack_method == AckMethod.AUTO:
+                        self.ack(method.delivery_tag)
+                    return result
+                except Exception as e:
+                    if ack_method == AckMethod.AUTO:
+                        self.nack(method.delivery_tag, requeue=False)
+                    raise e
 
-            self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=ack_method == AckMethod.ON_FETCH)
+            def callback(*args, **kwargs):
+                thread = Thread(target=do_works, args=args, kwargs=kwargs)
+                self._threads.append(thread)
+                thread.start()
+                return thread
+
+            self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=ack_method == AckMethod.INSTANTLY)
 
         return wrapper
